b9a002f52ad33f476aeba82aeef0518254fda083
mmgrillo <mmgrillo1@gmail.com>
2015-05-16 19:31:08 +0100

    made product validation case sensitive

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
 	validates :name, :description, :image_url, :key, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
-	validates :name, uniqueness: true
+	validates_uniqueness_of :name, case_sensitive: false
+	validates :key, uniqueness: true
 	validates :image_url, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG, or PNG image.'
