13ec2ed81556a6c677407f93d19d4648d24f6896
mmgrillo <mmgrillo1@gmail.com>
2015-05-20 18:41:36 +0100

    added sendgrid pass + user in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => ENV['app36620669@heroku.com'],
+  :password       => ENV['eqzzyvqa4329'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
