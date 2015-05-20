8692840c8bc7790cfe4c0c68efa8d15f2c691b7f
mmgrillo <mmgrillo1@gmail.com>
2015-05-20 18:29:59 +0100

    added Contact Form + Prepared Sendgrid Mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,20 @@
 class StaticPagesController < ApplicationController
   def index
   end
+  
   def landing_page
     @products = Product.limit(3)
   end
+  
+  ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver
+
+  def thank_you
+	  @name = params[:name]
+	  @email = params[:email]
+	  @message = params[:message]
+	  ActionMailer::Base.mail(:from => @email, 
+		  :to => 'mmgrillo1@gmail.com', 
+		  :subject => "A new contact form message from #{@name}", 
+		  :body => @message).deliver
+end
 end
