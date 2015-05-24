a7e770046d20caac55a5bd6f5dbdf7fd6cec8d04
mmgrillo <mmgrillo1@gmail.com>
2015-05-24 14:27:19 +0100

    changed sendgrid credentials from production.rb to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,13 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['app36620669@heroku.com'],
+  :password       => ENV['Benfica10'],
+  :domain         => 'monte-defesa.heroku.com',
+  :enable_starttls_auto => true
+}
