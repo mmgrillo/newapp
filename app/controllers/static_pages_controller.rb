0c4f0152a72f42e124612a8d09a8624556529ce2
mmgrillo <mmgrillo1@gmail.com>
2015-05-29 17:50:50 +0100

    Moved UserMailer to the user_mailer.rb file(from static_pages_controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,13 +6,11 @@ class StaticPagesController < ApplicationController
     @products = Product.limit(3)
   end
   
-  ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver
 
   def thank_you
 	  @name = params[:name]
 	  @email = params[:email]
 	  @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver
 	  ActionMailer::Base.mail(:from => @email, 
 		  :to => 'mmgrillo1@gmail.com', 
 		  :subject => "A new contact form message from #{@name}", 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,14 @@ class UserMailer < ApplicationMailer
 	def contact_form(email, name, message)
 		@message = message
 		mail(:from => email,
-									:to => 'your-email@example.com',
+									:to => 'mmgrillo1@gmail.com',
 									:subject => "A new contact form message from #{name}")
 	end
+
+	def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver
+  end
 end
