1e1f9f4ca9e9db269345db3cd63f4f7c01ffc452
mmgrillo <mmgrillo1@gmail.com>
2015-05-31 18:59:24 +0100

    Fixed User_mailer and added a welcome email for new signups

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,17 @@
 class UserMailer < ApplicationMailer
 	default from: "manuel@monte-defesa.com"
+	
 	def contact_form(email, name, message)
 		@message = message
 		mail(:from => email,
-									:to => 'mmgrillo1@gmail.com',
-									:subject => "A new contact form message from #{name}")
+					:to => 'mmgrillo1@gmail.com',
+					:subject => "A new contact form message from #{name}")
 	end
+
+	def welcome_email(user)
+		@user = user
+		@url = "/sign_up"
+		mail(to: @user.email, subject: "Thanks for registering.")
+	end
+
 end
