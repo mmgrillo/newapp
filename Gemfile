3c142eff4664023b6113a63361d4fb777199ac51
mmgrillo <mmgrillo1@gmail.com>
2015-06-01 11:54:56 +0100

    Added Mailcatcher Gem and Quiet_Assets Gem

--- Gemfile
+++ Gemfile
@@ -39,6 +39,10 @@ group :production do
 	gem 'rails_12factor'
 end
 
+group :development do
+	gem 'mailcatcher'
+end	
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
@@ -48,5 +52,6 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
-end
 
+  gem 'quiet_assets'
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options   = { :host => "localhost:3000", :only_path => false }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => '127.0.0.1', :port => 1025} # the mailcatcher # http://mailcatcher.me/
+  config.action_mailer.asset_host = 'http://localhost:3000'
 end
