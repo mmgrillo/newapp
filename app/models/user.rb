4a776eafa908d0ced3a6ca2d4040e12b31ac361a
mmgrillo <mmgrillo1@gmail.com>
2015-06-13 18:04:39 +0100

    added strong password to devise forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :orders
+
+	validate :password_complexity
+
+	def password_complexity													 
+		  if password.present? and not password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d). /)
+      errors.add :password, "must include at least one lowercase letter, one uppercase letter, and one digit"
+    end
+  end
 end
