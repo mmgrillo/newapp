327405db01f6fea5a674f57924131b08f6971bb1
mmgrillo <mmgrillo1@gmail.com>
2015-06-14 16:56:48 +0100

    disabled set_comment for all actions except destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_comment, only: [:destroy]
 
   def create
     @product = Product.find(params[:product_id])
